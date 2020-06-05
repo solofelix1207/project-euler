afcf7e8bf7c407f638a4b99020818bbcefb81fc7
solofelix1207 <sfsobeng@gmail.com>
2020-06-05 23:30:26 +0000

    Multiples of 3 and 5
    My solution to Problem 1 - Multiples of 3 and 5

--- problem-1.py
+++ problem-1.py
@@ -3,9 +3,8 @@
 # If we list all the natural numbers below 10 that are 
 # multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 # Find the sum of all the multiples of 3 or 5 below 1000.
-from time import time as t
 
-limit = int(input('\nEnter the number you want to check for: '))
+limit = int(input('\nEnter the number to begin: '))
 limit_range = list(range(1,limit))
 
 fin_list = []
