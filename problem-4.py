6beeafa9558c4eaa49af43dcb4ba132ce2d8ade0
solofelix1207 <sfsobeng@gmail.com>
2020-06-05 19:37:39 +0000

    Largest palindrome product
    My solution to Problem 4 - Largest palindrome product

--- problem-4.py
+++ problem-4.py
@@ -1,11 +1,7 @@
 from time import time as t
 
 print('Welcome to My Solution to Project Euler - Problem 4\n')
-
-
-
 ndigi = int(input('Please enter number of digits (n): ')) 
-
 start = t()     #record the start time
 k1 = int('1' + ((ndigi-1)*'0'))    #use k1 and k2 to generate the start and end of the selected digits
 k2 = int('1' + (ndigi*'0'))         #example for 2, this will be 100 to 1000 (range will correct it to 99)         
@@ -16,8 +12,6 @@ palinum = []
 for x in range (k1,k2):          #use this loop to generate the list of all the products of all the multiplications
     for y in range(k1,k2):
         results.append(x*y)
-
-
 
 for num in results:
     num = str(num)   #convert to string so we can loop through
@@ -33,7 +27,6 @@ for num in results:
     if halfrange == halfrange2:      #check for equality
         num = int(num)
         palinum.append(num)        #store the equal numbers 
-
 
 palinum.sort()   #sort from lowest to highest and in below print statement, pick the last item which is the highest.
 print(f'The largest palindrome made from the product of two {ndigi}-digit numbers is: {palinum[-1]}')
