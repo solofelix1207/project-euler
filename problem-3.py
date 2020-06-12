3835a715b69dd49577d3d0cf56a4e7a921310643
solofelix1207 <sfsobeng@gmail.com>
2020-06-12 14:45:39 +0000

    Problem-3
    Largest prime factor

---
+++ problem-3.py
@@ -0,0 +1,13 @@
+#Problem 3 - Largest prime factor
+#In Python using sympy
+#
+
+from time import time as t
+import sympy
+
+start = t()
+print(max(sympy.primefactors(600851475143)))
+
+end = t()
+
+print(f'time taken {round((end-start),2)} seconds')
