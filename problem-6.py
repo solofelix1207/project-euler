402b79a34906c5ab2347f0a5dbaab71462addb25
solofelix1207 <sfsobeng@gmail.com>
2020-07-15 13:55:32 +0000

    Added index.html
    some description

---
+++ problem-6.py
@@ -0,0 +1,14 @@
+
+
+# final_numbers = []
+# squares = []
+raw_sum=0
+sum_individual_squares =0
+
+for i in range(1,101):
+    raw_sum = raw_sum + i
+    sum_individual_squares = sum_individual_squares + (i**2)
+
+difference = (raw_sum**2) - sum_individual_squares
+
+print(difference)
