c7d7ed67ea8a1d8ac42248012e552b4c40f0177f
solofelix1207 <sfsobeng@gmail.com>
2020-07-15 14:02:34 +0000

    Sum square difference
    Sum square difference-my solution

--- problem-6.py
+++ problem-6.py
@@ -1,7 +1,3 @@
-
-
-# final_numbers = []
-# squares = []
 raw_sum=0
 sum_individual_squares =0
 
@@ -9,6 +5,6 @@ for i in range(1,101):
     raw_sum = raw_sum + i
     sum_individual_squares = sum_individual_squares + (i**2)
 
-difference = (raw_sum**2) - sum_individual_squares 
+difference_of_sum = raw_sum**2 - sum_individual_squares 
 
-print(difference)
+print(difference_of_sum)
